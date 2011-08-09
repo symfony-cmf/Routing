a0c429eca9871cab23394f07c93555bab58906dc
Henrik Bjørnskov <henrik@bearwoods.dk>
2011-08-09 10:26:31 +0200

    Fix namespace and class name

--- DependencyInjection/ChainRoutingExtension.php
+++ DependencyInjection/ChainRoutingExtension.php
@@ -5,7 +5,7 @@ namespace Symfony\Bundle\ChainRoutingBundle\DependencyInjection;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\HttpKernel\DependencyInjection\Extension;
-use Symfony\Component\DependencyInjection\Loader;
+use Symfony\Component\DependencyInjection\Loader\XmlFileLoader;
 
 /**
  * This is the class that loads and manages your bundle configuration
@@ -19,7 +19,7 @@ class ChainRoutingExtension extends Extension
      */
     public function load(array $configs, ContainerBuilder $container)
     {
-        $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
+        $loader = new XmlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
         $loader->load('chain_routing.xml');
     }
 }
