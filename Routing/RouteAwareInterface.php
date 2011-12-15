05d389da4d2f100c5b5e4495dc434a66018af399
dbu <david.buchmann@liip.ch>
2011-12-15 17:56:32 +0100

    adding an interface for content that supports routes

--- Routing/RouteAwareInterface.php
+++ Routing/RouteAwareInterface.php
@@ -3,39 +3,20 @@
 namespace Symfony\Cmf\Bundle\ChainRoutingBundle\Routing;
 
 /**
- * Documents for entries in the routing table need to implement this interface
- * so the DoctrineRouter can handle them.
+ * Interface to be implemented by content that wants to be compatible with the
+ * DoctrineRouter
  */
-interface RouteObjectInterface
+interface RouteAwareInterface
 {
     /**
-     * Get the content document this route entry stands for. If non-null,
-     * the ControllerClassResolver uses it to identify a controller and
-     * the content is passed to the controller.
+     * Get the routes that point to this content.
      *
-     * If there is no specific content for this url (i.e. its an "application"
-     * page), may return null.
+     * Note: For PHPCR, as explained in RouteObjectInterface the route must use
+     * the routeContent field to store the reference to the content so you can
+     * get the routes with @Referrers(filter="routeContent")
      *
-     * To interoperate with the standard Symfony\Cmf\Bundle\ContentBundle\Document\StaticContent
-     * the instance MUST store the property in the field <code>routeContent</code>
-     * to have referrer resolution work.
-     *
-     * @return object the document or entity this route entry points to
+     * @return array of RouteObjectInterface instances that point to this content
      */
-    function getRouteContent();
-
-    /**
-     * Get the path for this route. The path may be prepended with a prefix -
-     * in the case of phpcr the ID (full repository path) will work if you
-     * correctly configure the DoctrineRouter with that prefix.
-     */
-    function getPath();
-
-    /**
-     * To work with the ControllerAliasResolver, this must at least contain
-     * the field 'type' with a value from the controllers_by_alias mapping
-     *
-     * @return array Information for the ControllerResolver
-     */
-    function getRouteDefaults();
+    function getRoutes();
 }
+
