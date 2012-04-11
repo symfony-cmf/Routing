47cee71de89349adf9bb4126dc47c3eda4e0091b
dbu <david.buchmann@liip.ch>
2012-04-11 09:03:15 +0200

    use composer in the chainroutingbundle and prepare for functional tests

--- Tests/bootstrap.php
+++ Tests/bootstrap.php
@@ -1,7 +1,17 @@
 <?php
 
+use Symfony\Component\ClassLoader\UniversalClassLoader;
+use Doctrine\Common\Annotations\AnnotationRegistry;
+
 if (file_exists($file = __DIR__.'/autoload.php')) {
     require_once $file;
 } elseif (file_exists($file = __DIR__.'/autoload.php.dist')) {
     require_once $file;
 }
+
+$loader = new UniversalClassLoader();
+AnnotationRegistry::registerLoader(function($class) use ($loader) {
+    $loader->loadClass($class);
+    return class_exists($class, false);
+});
+AnnotationRegistry::registerFile(__DIR__.'/../vendor/doctrine/phpcr-odm/lib/Doctrine/ODM/PHPCR/Mapping/Annotations/DoctrineAnnotations.php');
