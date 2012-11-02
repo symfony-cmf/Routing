4a5851d12e1e1be29c22dfc31201f2c53a4c3cd8
Larry Garfield <larry@garfieldtech.com>
2012-11-02 13:34:41 -0500

    Add initial NestedMatcher architecture, as developed by Drupal.

---
+++ NestedMatcher/PartialMatcher.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Symfony\Cmf\Component\Routing\NestedMatcher;
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\RouteCollection;
+
+/**
+ * Utility base class for partial matchers.
+ */
+abstract class PartialMatcher implements PartialMatcherInterface {
+
+  /**
+   * The RouteCollection this matcher should match against.
+   *
+   * @var \Symfony\Component\Routing\RouteCollection
+   */
+  protected $routes;
+
+  /**
+   * Sets the route collection this matcher should use.
+   *
+   * @param \Symfony\Component\Routing\RouteCollection $collection
+   *   The collection against which to match.
+   *
+   * @return \Drupal\Core\Routing\PartialMatcherInterface
+   *   The current matcher.
+   */
+  public function setCollection(RouteCollection $collection) {
+    $this->routes = $collection;
+
+    return $this;
+  }
+
+}
