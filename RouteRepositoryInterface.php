bfedccc19b53cd7aec0dd940286c01f488f7fccb
David Buchmann <david.buchmann@liip.ch>
2012-11-20 07:15:38 +0100

    allow repository to throw ResourceNotFoundException

--- RouteRepositoryInterface.php
+++ RouteRepositoryInterface.php
@@ -5,6 +5,7 @@ namespace Symfony\Cmf\Component\Routing;
 use Symfony\Component\Routing\Route;
 use Symfony\Component\Routing\RouteCollection;
 use Symfony\Component\Routing\Exception\RouteNotFoundException;
+use Symfony\Component\Routing\Exception\ResourceNotFoundException;
 use Symfony\Component\HttpFoundation\Request;
 
 /**
@@ -22,16 +23,23 @@ interface RouteRepositoryInterface
      * must be instanceof the core symfony route. The classes may also implement
      * RouteObjectInterface to link to a content document.
      *
-     * This method may not throw an exception based on implementation specific
-     * restrictions on the url. That case is considered a not found - returning
-     * an empty collection. Exceptions are only used to abort the whole request in
-     * case something is seriously broken, like the storage backend being down.
+     * The repository <em>may</em> throw a ResourceNotFoundException if the route
+     * collection is empty. It can do this to provide more detailed information
+     * on what it was looking for, as the DynamicRouter just has a generic message.
+     *
+     * This method may not throw other exception than ResourceNotFoundException
+     * based on implementation specific restrictions on i.e. the url. That case
+     * is considered a not found. Exceptions are only used to abort the whole
+     * request in case something is seriously broken, like the storage backend
+     * being down.
      *
      * @param Request $request
      *
      * @return RouteCollection with all urls that
      *      could potentially match $url. Empty collection if nothing can match.
      *
+     * @throws ResourceNotFoundException may be thrown instead of returning an
+     *      empty route collection.
      * @throws \Exception if the underlying storage has an error
      */
     public function getRouteCollectionForRequest(Request $request);
