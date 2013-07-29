4f305cc2f22f6b281ef0fa55e88d35c66f5e5e7e
Trent Steel <tsteel@vivogroup.com.au>
2013-07-30 08:14:01 +1000

    add match Events to DynamicRouter

---
+++ Event/Events.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Symfony\Cmf\Component\Routing\Event;
+
+final class Events
+{
+    const PRE_DYNAMIC_MATCH = 'cmf_routing.pre_dynamic_match';
+
+    const PRE_DYNAMIC_MATCH_REQUEST = 'cmf_routing.pre_dynamic_match_request';
+}
---
+++ Event/RouterMatchEvent.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Symfony\Cmf\Component\Routing\Event;
+
+use Symfony\Component\EventDispatcher\Event;
+
+class RouterMatchEvent extends Event
+{
+}
