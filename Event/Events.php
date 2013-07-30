c8de775f5d9fdbd15c36e5878cf7a254e79b7cda
Trent Steel <tsteel@vivogroup.com.au>
2013-07-31 08:22:51 +1000

    add comments

--- Event/Events.php
+++ Event/Events.php
@@ -4,7 +4,13 @@ namespace Symfony\Cmf\Component\Routing\Event;
 
 final class Events
 {
+    /**
+     * Fired before a path is matched in \Symfony\Cmf\Component\Routing\DynamicRouter#match
+     */
     const PRE_DYNAMIC_MATCH = 'cmf_routing.pre_dynamic_match';
 
+    /**
+     * Fired before a Request is matched in \Symfony\Cmf\Component\Routing\DynamicRouter#match
+     */
     const PRE_DYNAMIC_MATCH_REQUEST = 'cmf_routing.pre_dynamic_match_request';
 }
