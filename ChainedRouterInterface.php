ca7621aebf542ed5033f97523a7b4652b238b129
WouterJ <waldio.webdesign@gmail.com>
2014-03-09 17:41:52 +0100

    Updated license headers
    - Bumped year
    - Added missing headers
    
    refs symfony-cmf/symfony-cmf#184

--- ChainedRouterInterface.php
+++ ChainedRouterInterface.php
@@ -3,7 +3,7 @@
 /*
  * This file is part of the Symfony CMF package.
  *
- * (c) 2011-2013 Symfony CMF
+ * (c) 2011-2014 Symfony CMF
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
