8c41a43c31977fdeb5feee09cd450725bdc9bb8e
WouterJ <waldio.webdesign@gmail.com>
2015-10-17 00:20:58 +0200

    Update year in the license headers

--- PagedRouteProviderInterface.php
+++ PagedRouteProviderInterface.php
@@ -1,9 +1,16 @@
 <?php
-
 /**
  * This file is part of the Symfony CMF package.
  *
  * (c) 2011-2014 Symfony CMF
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+/*
+ * This file is part of the Symfony CMF package.
+ *
+ * (c) 2011-2015 Symfony CMF
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
